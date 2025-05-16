3e1f54b5e84bd6a01a1fba27abc00ee1d0253b4c
Flesh-Gordon <thefleshgordon@gmail.com>
2025-05-16 12:03:34 -0400

    Update comment_handler.py

--- comment_handler.py
+++ comment_handler.py
@@ -1,11 +1,13 @@
 import re
 from youtube_client import get_video_details
 from formatter import format_reply
+from spotify_client import get_genres_for_song
 
 def handle_comment(comment):
     if re.match(r"(?i)^YachtThot play ", comment.body):
         query = re.sub(r"(?i)^YachtThot play ", "", comment.body).strip()
         video = get_video_details(query)
         if video:
-            reply = format_reply(video)
+            genres = get_genres_for_song(video["title"])
+            reply = format_reply(video, genres=genres)
             comment.reply(reply)
