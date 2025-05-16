fe47b26e669e722246aa81f77005596c59748b11
Flesh-Gordon <thefleshgordon@gmail.com>
2025-05-16 10:47:16 -0400

    Create snark_pool.py

---
+++ snark_pool.py
@@ -0,0 +1,12 @@
+import random
+
+SNARK_REPLIES = [
+    "Nice choice... if you're legally tone-deaf.",
+    "Bold of you to post this in public.",
+    "YachtThot we were better than this.",
+    "Just when I thought we hit rock bottom, you dropped this.",
+    "If cringe had a soundtrack, this would be it."
+]
+
+def get_snark_reply():
+    return random.choice(SNARK_REPLIES)
