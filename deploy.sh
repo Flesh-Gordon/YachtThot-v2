2a20c744965eeee6acaa7e6e0fa2208f7e66bdcf
Flesh-Gordon <thefleshgordon@gmail.com>
2025-05-16 12:06:35 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/thefleshgordon/YachtThot-v2
-git pull origin main
-systemctl restart yachtthot-bot.service
+git fetch origin snark-engine
+git checkout snark-engine
+git reset --hard
+git pull origin snark-engine --rebase
+sudo systemctl restart yachtthot-bot.service
