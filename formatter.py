a0997cf8c8dd8d783725a0c1c24dc00c28c2cd60
Flesh-Gordon <thefleshgordon@gmail.com>
2025-05-16 10:49:52 -0400

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -1,11 +1,18 @@
 from utils import parse_duration, build_media_bar
+import random
+from snark_pool import get_snark_reply
 
 def format_reply(video):
     duration = parse_duration(video["duration"])
     bar = build_media_bar(duration)
+
+    snark = get_snark_reply() if random.random() < 0.25 else ""
+
     return f"""**NOW PLAYING:**
 
 [{video['title']}](https://www.youtube.com/watch?v={video['id']})
 
 {bar}  0:01 / {duration}
+
+{snark}
 """
