746ef4e366244d095b7510ae5aa38e2c1efa1f8c
Flesh-Gordon <thefleshgordon@gmail.com>
2025-05-17 03:46:32 +0000

    Webhook deploy test

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/thefleshgordon/YachtThot-v2/venv
+set -gx VIRTUAL_ENV /home/thefleshgordon/reddit_bot/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
