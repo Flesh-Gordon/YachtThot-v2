207bac90ea381646db8c03d25a021864e49e6452
Flesh-Gordon <thefleshgordon@gmail.com>
2025-05-17 07:44:13 -0400

    Update env_loader.py

--- env_loader.py
+++ env_loader.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 import os
 
-def load_env():
-    load_dotenv()
+def load_env(path=".env"):
+    load_dotenv(dotenv_path=path)
     return {
         "REDDIT_CLIENT_ID": os.getenv("REDDIT_CLIENT_ID"),
         "REDDIT_SECRET": os.getenv("REDDIT_SECRET"),
@@ -10,5 +10,7 @@ def load_env():
         "REDDIT_PASSWORD": os.getenv("REDDIT_PASSWORD"),
         "USER_AGENT": os.getenv("USER_AGENT"),
         "YOUTUBE_API_KEY": os.getenv("YOUTUBE_API_KEY"),
+        "SPOTIFY_CLIENT_ID": os.getenv("SPOTIFY_CLIENT_ID"),
+        "SPOTIFY_CLIENT_SECRET": os.getenv("SPOTIFY_CLIENT_SECRET"),
         "SUBREDDIT": os.getenv("SUBREDDIT"),
     }
