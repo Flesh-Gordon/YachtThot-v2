04a532852b714d0f4fc91a2d4dd909acfcc90411
Flesh-Gordon <thefleshgordon@gmail.com>
2025-05-16 22:20:07 -0400

    Create genre_detector.py

---
+++ genre_detector.py
@@ -0,0 +1,13 @@
+from spotify_client import get_genres_for_song
+from lastfm_client import get_lastfm_tags
+
+def detect_genre_tags(track_title):
+    # Get genres from Spotify (artist-level)
+    spotify_genres = get_genres_for_song(track_title)
+
+    # Get tags from Last.fm (track-level)
+    lastfm_tags = get_lastfm_tags(track_title)
+
+    # Combine and deduplicate
+    combined = list(set(spotify_genres + lastfm_tags))
+    return combined
