cafd4cc471a01a8538bf54c562203ecb0b2d216a
Flesh-Gordon <thefleshgordon@gmail.com>
2025-05-17 07:51:32 -0400

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -4,11 +4,11 @@ def format_reply(video, snark_override=None):
     duration = parse_duration(video["duration"])
     bar = build_media_bar(duration)
 
-    snark_line = f"\n\n{snark_override}" if snark_override else ""
-
     return f"""**NOW PLAYING:**
 
 [{video['title']}](https://www.youtube.com/watch?v={video['id']})
 
-{bar}  0:01 / {duration}{snark_line}
+{bar}  0:01 / {duration}
+
+{snark_override if snark_override else ""}
 """
